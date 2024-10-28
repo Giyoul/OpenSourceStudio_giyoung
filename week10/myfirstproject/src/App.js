1d5b0d198ffb0224fd9cb71bfda71169e4255ad8
Giyoul <cla12093@gmail.com>
2024-10-28 10:51:59 +0900

    add

---
+++ week10/myfirstproject/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './Component1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          20241028 react oss class
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Component1></Component1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
